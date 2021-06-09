2a6cd898b40f1b50b81cb61c6f4995fdf94debd9
tuxuser <462620+tuxuser@users.noreply.github.com>
2021-06-09 04:04:13 +0200

    Get XeLL to build again
    * Remove all filesystems except FAT
    * Generate more detailed git-build-string for dist-archive

--- Makefile_lv2.mk
+++ Makefile_lv2.mk
@@ -37,7 +37,7 @@ LDFLAGS	=	-g $(MACHDEP) -Wl,--gc-sections -Wl,-Map,$(notdir $@).map
 #---------------------------------------------------------------------------------
 # any extra libraries we wish to link with the project
 #---------------------------------------------------------------------------------
-LIBS	:=	-lxenon -lfat -lext2fs -lntfs -lxtaf -lm
+LIBS	:=	-lxenon -lm -lfat # -lext2fs -lntfs -lxtaf
 
 #---------------------------------------------------------------------------------
 # list of directories containing libraries, this must be the top level containing
